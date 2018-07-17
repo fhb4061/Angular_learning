a55f85d9aff96552770bbf5c9ac3eb00b75e009b
Jonathan <fhb4061@autuni.ac.nz>
2018-07-17 21:24:57 +1200

    Successfully connected the components - search-city and detials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchCityComponent} from './search-city/search-city.component';
+import { DetailsComponent } from './details/details.component';
 
 //define array of routes
 const routes: Routes = [
   {
     path: 'search',
     component: SearchCityComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SearchCityComponent } from './search-city/search-city.component';
+import { DetailsComponent } from './details/details.component';
 
 import { HttpClientModule } from '@angular/common/http';
 
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     SidebarComponent,
-    SearchCityComponent
+    SearchCityComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
