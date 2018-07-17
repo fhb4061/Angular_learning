69572ed11517814efb11cacd90dbb68a1b9d1bdd
Jonathan <fhb4061@autuni.ac.nz>
2018-07-17 16:12:55 +1200

    adding in the HttpClientModule to communicate with an API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SearchCityComponent } from './search-city/search-city.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
